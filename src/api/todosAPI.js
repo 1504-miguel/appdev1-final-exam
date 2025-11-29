5587f47cb7997db32eaf4e5afeac3b21ee220cbc
Nathaniel Miguel David <miguelcastrodavid@gmail.com>
2025-11-29 13:22:39 +0800

    Implement API helper using axios

--- src/api/todosAPI.js
+++ src/api/todosAPI.js
@@ -1,28 +1,31 @@
+import axios from "axios";
+
 export const getTodosAPI = function () {
-    return fetch(`https://jsonplaceholder.typicode.com/todos?_limit=10`)
-    .then(res => res.json())
-}
+  return axios
+    .get("https://jsonplaceholder.typicode.com/todos", {
+      params: { _limit: 10 }
+    })
+    .then(res => res.data);
+};
+
 export const addTodoAPI = function (todo) {
-    return fetch(`https://jsonplaceholder.typicode.com/todos`, {
-        method: "POST",
-        body: JSON.stringify(todo),
-        headers: { "Content-Type": "application/json" }
+  return axios
+    .post("https://jsonplaceholder.typicode.com/todos", todo, {
+      headers: { "Content-Type": "application/json" }
     })
-    .then(res => res.json())
-}
+    .then(res => res.data);
+};
 
-export const updateTodoAPI = function(todo) {
-    return fetch(`https://jsonplaceholder.typicode.com/todos/${todo.id}`, {
-        method: "PUT",
-        body: JSON.stringify(todo),
-        headers: { "Content-Type": "application/json" }
+export const updateTodoAPI = function (todo) {
+  return axios
+    .put(`https://jsonplaceholder.typicode.com/todos/${todo.id}`, todo, {
+      headers: { "Content-Type": "application/json" }
     })
-    .then(res => res.json())
-}
+    .then(res => res.data);
+};
 
-export const deleteTodoAPI = function(id) {
-    return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
-        method: "DELETE"
-    })
-    .then(() => id)
-}
+export const deleteTodoAPI = function (id) {
+  return axios
+    .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(() => id);
+};
